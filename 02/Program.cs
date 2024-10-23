dc60889aa18c96408a2cec5b5af88523b430143c
Alexander Bukhanenko <alexandr.buhanenko@gmail.com>
2024-10-23 17:06:42 +0300

    Add project files.

---
+++ 02/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+
+    public class MyList<T> : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+                yield return array[i];
+            yield break;
+        }
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        private T[] array;
+
+        public int Add(T value)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = value;
+            return array.Length - 1;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= array.Length - 1)
+                    return array[index];
+                else
+                    throw new Exception("Index out of array");
+            }
+            set
+            {
+                if (index >= 0 && index <= array.Length - 1)
+                    array[index] = value;
+                else
+                    throw new Exception("Index out of array");
+            }
+        }
+        public void Clear()
+        {
+            Array.Resize(ref array, 0);
+        }
+        public int Count => array.Length;
+
+
+
+        //void Insert(int index, T value)
+        //{ }
+        //void RemoveAt(int index)
+        //{ }
+    }
+
+
+    internal class Program
+    {
+        
+
+        
+
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(100);
+            myList.Add(200);
+            myList.Add(300);
+            myList.Add(400);
+
+            foreach (int a in myList)
+                Console.WriteLine(a);
+
+            Console.WriteLine(new string('-', 30));
+
+            foreach (int a in myList)
+                Console.WriteLine(a);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 04/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02;
+
+
+namespace _04
+{
+    public static class ExClass
+    {
+        public static T[] GetArray<T>(this IEnumerable list)
+        {
+
+
+            int Count = 0;
+            foreach(var item in list)
+                Count++;
+            // або
+            // 
+            // ICollection col = list as ICollection;
+            // Count = col.Count;
+
+            // або 
+            // Count = (list as MyList<T>).Count
+
+            T[] arr = new T[Count];
+
+            int i = 0;
+            foreach (var item in list)
+                arr[i++] = (T)item;
+
+            return arr;
+        }
+    }
+
+    internal class Program
+    {
+
+        
+        
+
+        static void Main(string[] args)
+        {
+
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("zero");
+            myList.Add("one");
+            myList.Add("two");
+            myList.Add("three");
+
+            string[] arr = myList.GetArray<string>();
+
+
+
+            foreach (var item in arr)
+                Console.WriteLine(item);
+
+
+
+
+
+
+
+
+            int[] a = { 1, 2, 3 };
+            int[] arr1 = a.GetArray<int>();
+
+
+            MyList<B> l1 = new MyList<B>();
+            l1.Add(new B());
+            l1.Add(new B());
+            l1.Add(new B());
+            l1.Add(new B());
+
+            A[] arr2 = l1.GetArray<A>();
+
+            Console.ReadKey();
+        }
+        class A
+        { }
+        class B : A { }
+    }
+}
